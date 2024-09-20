e55926f0fe7c9dda24f1154e96fca44c947f4b8a
Maira Wenzel <mairaw@microsoft.com>
2024-09-20 16:01:02 -0700

    Add project files.

---
+++ demo-ai/Program.cs
@@ -0,0 +1,15 @@
+﻿using OpenAI;
+using System.ClientModel;
+
+string? apiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
+
+if (string.IsNullOrEmpty(apiKey))
+{
+    throw new InvalidOperationException("The OpenAI API key is not set in the environment variables.");
+}
+
+OpenAIClient client = new OpenAIClient(new ApiKeyCredential(apiKey));
+var chatService = client.GetChatClient("gpt-4o-mini");
+
+var result = await chatService.CompleteChatAsync("Qual é a cor do céu?");
+Console.WriteLine(result.Value);
